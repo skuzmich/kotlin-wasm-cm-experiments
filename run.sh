567d7160680250845755bcd33743f522f66d8d02
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2024-05-24 01:03:07 +0200

    Stop using local wit-bindgen

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ cargo build --target wasm32-wasi --release -q && \
 wasm-tools component new ./target/wasm32-wasi/release/example.wasm -o ../build/out/dependencies/example.wasm --adapt ../$WASI_ADAPTER)
 
 # Generate WIT bindings for Kotlin
-cargo run --bin wit-bindgen --manifest-path /Users/skuzmich/work/wit-bindgen/Cargo.toml kotlin ./wit --out-dir src/wasmWasiMain/kotlin/bindings
+wit-bindgen kotlin ./wit --out-dir src/wasmWasiMain/kotlin/bindings
 java -jar ./ktfmt-0.47-jar-with-dependencies.jar ./src/wasmWasiMain/kotlin/bindings
 
 # Compile Kotlin code
